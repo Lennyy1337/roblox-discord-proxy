d67d88eddd3a9a5c0f3f39b5665d01418f7fff6e
Lenny <77895936+Lennyy1337@users.noreply.github.com>
2024-02-12 18:45:44 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import fastify from 'fastify'
+import axios, {isCancel, AxiosError} from 'axios';
+const server = fastify({ logger: false })
+
+server.get('/ping', function (request, reply) {
+    reply.send({ error: false, ready: request.server !== null })
+})
+
+server.post('/api/webhooks/:webhook/:token', async (request, reply) => {
+    const { webhook, token } = (request.params as any);
+    const data = request.body
+    const res = await axios.post('https://canary.discord.com/api/webhooks/' + webhook + '/' + token, data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    reply.send(res.data + res.status)
+})
+
+server.listen({ port: 3000 }, function (err, address) {
+    console.log("Ready!")
+    if (err) {
+      server.log.error(err)
+    }
+  })
